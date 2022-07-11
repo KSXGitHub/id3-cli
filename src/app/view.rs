cdeee90c6372ca4fee9b0c0a25fbad421961ae57
khai96_ <hvksmr1996@gmail.com>
2022-07-11 16:49:02 +0700

    Implement remaining text fields

--- src/app/view.rs
+++ src/app/view.rs
@@ -7,15 +7,22 @@ use pipe_trait::Pipe;
 
 pub fn view(args: ViewCmd) -> Result<(), String> {
     match args {
-        Field::Text(Text::Title(TextViewArgs { input_audio })) => {
-            let tag = input_audio
-                .pipe(Tag::read_from_path)
-                .map_err(|e| e.to_string())?;
-            if let Some(title) = tag.title() {
-                println!("{title}");
-            }
-            Ok(())
-        }
+        Field::Text(Text::Title(args)) => view_text(args, Tag::title),
+        Field::Text(Text::Artist(args)) => view_text(args, Tag::artist),
+        Field::Text(Text::Album(args)) => view_text(args, Tag::album),
+        Field::Text(Text::AlbumArtist(args)) => view_text(args, Tag::album_artist),
+        Field::Text(Text::Genre(args)) => view_text(args, Tag::genre),
         _ => unimplemented!(),
     }
+}
+
+fn view_text(args: TextViewArgs, get: impl FnOnce(&Tag) -> Option<&str>) -> Result<(), String> {
+    let TextViewArgs { input_audio } = args;
+    let tag = input_audio
+        .pipe(Tag::read_from_path)
+        .map_err(|e| e.to_string())?;
+    if let Some(title) = get(&tag) {
+        println!("{title}");
+    }
+    Ok(())
 }
