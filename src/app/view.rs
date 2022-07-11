1fc4f3f0c8a664a9b668c887a61ea048ffc8e83b
khai96_ <hvksmr1996@gmail.com>
2022-07-11 16:29:32 +0700

    Implement `view title`

--- src/app/view.rs
+++ src/app/view.rs
@@ -1,6 +1,19 @@
-use crate::args::view::ViewCmd;
+use crate::args::{
+    field::{Field, Frame, Text},
+    view::{CommentViewArgs, PictureViewArgs, TextViewArgs, ViewCmd},
+};
+use id3::{Tag, TagLike};
+use pipe_trait::Pipe;
 
 pub fn view(args: ViewCmd) -> Result<(), String> {
-    dbg!(args);
-    unimplemented!()
+    match args {
+        Field::Text(Text::Title(TextViewArgs { input })) => {
+            let tag = input.pipe(Tag::read_from_path).map_err(|e| e.to_string())?;
+            if let Some(title) = tag.title() {
+                println!("{title}");
+            }
+            Ok(())
+        }
+        _ => unimplemented!(),
+    }
 }
