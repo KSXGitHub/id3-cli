a453e54f195cdfe674645409627e82bb3cfb117a
khai96_ <hvksmr1996@gmail.com>
2022-07-11 21:20:00 +0700

    refactor: Rearrange

--- src/text_format.rs
+++ src/text_format.rs
@@ -15,8 +15,8 @@ impl TextFormat {
     pub fn serialize(self, value: &impl Serialize) -> Result<String, String> {
         match self {
             TextFormat::Json => serde_json::to_string_pretty(value).map_err(|e| e.to_string()),
-            TextFormat::Toml => toml::to_string_pretty(value).map_err(|e| e.to_string()),
             TextFormat::Yaml => serde_yaml::to_string(value).map_err(|e| e.to_string()),
+            TextFormat::Toml => toml::to_string_pretty(value).map_err(|e| e.to_string()),
         }
     }
 }
