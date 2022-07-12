074b6c60f0280594d418d661c5916cebadd5d9e8
khai96_ <hvksmr1996@gmail.com>
2022-07-12 16:32:38 +0700

    test: Add 2 more cases

--- tests/get.rs
+++ tests/get.rs
@@ -1,8 +1,44 @@
 pub mod _utils;
 
-use _utils::{audio3, u8v_to_string, Exe, WORKSPACE};
+use _utils::{audio1, audio2, audio3, u8v_to_string, Exe, WORKSPACE};
 use command_extra::CommandExtra;
 use std::process::Output;
+
+#[test]
+fn title_empty1() {
+    let Output {
+        status,
+        stdout,
+        stderr,
+    } = Exe::new(WORKSPACE)
+        .cmd
+        .with_arg("get")
+        .with_arg("title")
+        .with_arg(audio1())
+        .output()
+        .expect("execute command");
+    assert!(status.success());
+    assert!(stderr.is_empty());
+    assert_eq!(u8v_to_string(&stdout), "");
+}
+
+#[test]
+fn title_positive2() {
+    let Output {
+        status,
+        stdout,
+        stderr,
+    } = Exe::new(WORKSPACE)
+        .cmd
+        .with_arg("get")
+        .with_arg("title")
+        .with_arg(audio2())
+        .output()
+        .expect("execute command");
+    assert!(status.success());
+    assert!(stderr.is_empty());
+    assert_eq!(u8v_to_string(&stdout), "砕月\n");
+}
 
 #[test]
 fn title_positive3() {
