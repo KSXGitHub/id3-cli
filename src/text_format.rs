315a6f60417378a4f2336ab3b914df1c664d4b16
khai96_ <hvksmr1996@gmail.com>
2022-07-14 16:11:24 +0700

    test: TextFormat::serialize

--- src/text_format.rs
+++ src/text_format.rs
@@ -19,3 +19,96 @@ impl TextFormat {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::TextFormat;
+    use serde::Serialize;
+    use text_block_macros::text_block;
+
+    #[derive(Debug, Serialize)]
+    struct TestSubject {
+        number: i32,
+        string: &'static str,
+        array: &'static [TestSubject],
+    }
+
+    const TEST_SUBJECT: TestSubject = TestSubject {
+        number: 123,
+        string: "text",
+        array: &[
+            TestSubject {
+                number: 0,
+                string: "a",
+                array: &[],
+            },
+            TestSubject {
+                number: 1,
+                string: "a",
+                array: &[TestSubject {
+                    number: 0,
+                    string: "b",
+                    array: &[],
+                }],
+            },
+        ],
+    };
+
+    #[test]
+    fn json() {
+        let received = TextFormat::Json
+            .serialize(&TEST_SUBJECT)
+            .expect("serialize test subject");
+        let expected = text_block! {
+            r#"{"#
+            r#"  "number": 123,"#
+            r#"  "string": "text","#
+            r#"  "array": ["#
+            r#"    {"#
+            r#"      "number": 0,"#
+            r#"      "string": "a","#
+            r#"      "array": []"#
+            r#"    },"#
+            r#"    {"#
+            r#"      "number": 1,"#
+            r#"      "string": "a","#
+            r#"      "array": ["#
+            r#"        {"#
+            r#"          "number": 0,"#
+            r#"          "string": "b","#
+            r#"          "array": []"#
+            r#"        }"#
+            r#"      ]"#
+            r#"    }"#
+            r#"  ]"#
+            r#"}"#
+        };
+        eprintln!("RECEIVED:\n{received}");
+        assert_eq!(received, expected);
+    }
+
+    #[test]
+    fn yaml() {
+        let received = TextFormat::Yaml
+            .serialize(&TEST_SUBJECT)
+            .expect("serialize test subject");
+        let expected = text_block! {
+            "---"
+            "number: 123"
+            "string: text"
+            "array:"
+            "  - number: 0"
+            "    string: a"
+            "    array: []"
+            "  - number: 1"
+            "    string: a"
+            "    array:"
+            "      - number: 0"
+            "        string: b"
+            "        array: []"
+            ""
+        };
+        eprintln!("RECEIVED:\n{received}");
+        assert_eq!(received, expected);
+    }
+}
