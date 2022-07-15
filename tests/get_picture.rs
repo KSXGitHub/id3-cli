f1269f1e840d4c8b7bc901478c566859b4724c28
khai96_ <hvksmr1996@gmail.com>
2022-07-15 12:15:57 +0700

    test: `get picture file --id`

--- tests/get_picture.rs
+++ tests/get_picture.rs
@@ -164,6 +164,97 @@ picture_list!(picture_list_yaml_filled3: "audio3" --format=yaml => [
     },
 ]);
 
+macro_rules! picture_file_id {
+    (
+        $(#[$attributes:meta])*
+        $name:ident: $audio_path:literal --id=$id:literal => $expected:literal
+    ) => {
+        $(#[$attributes])*
+        #[test]
+        fn $name() {
+            let Exe { cmd, wdir } = Exe::temp_workspace();
+            let audio_path = wdir.join("assets").join($audio_path);
+            let image_path = wdir.join("exported-image");
+            let Output {
+                status,
+                stdout,
+                stderr,
+            } = cmd
+                .with_arg("get")
+                .with_arg("picture")
+                .with_arg("file")
+                .with_arg(&audio_path)
+                .with_arg(&image_path)
+                .with_arg("--id")
+                .with_arg($id)
+                .output()
+                .expect("execute command");
+
+            // for ease of debug
+            eprintln!("STDERR:\n{}", u8v_to_string(&stderr));
+            eprintln!("STDOUT:\n{}", u8v_to_string(&stdout));
+
+            // basic guarantees
+            assert!(status.success());
+            assert!(stdout.is_empty());
+            assert!(stderr.is_empty());
+
+            // compare hash
+            assert_eq!(sha256_file(image_path), $expected);
+        }
+    };
+}
+
+macro_rules! picture_file_id_fail {
+    (
+        $(#[$attributes:meta])*
+        $name:ident: $audio_path:literal --id=$id:literal => $expected:literal
+    ) => {
+        $(#[$attributes])*
+        #[test]
+        fn $name() {
+            let Exe { cmd, wdir } = Exe::temp_workspace();
+            let audio_path = wdir.join("assets").join($audio_path);
+            let image_path = wdir.join("exported-image");
+            let Output {
+                status,
+                stdout,
+                stderr,
+            } = cmd
+                .with_arg("get")
+                .with_arg("picture")
+                .with_arg("file")
+                .with_arg(&audio_path)
+                .with_arg(&image_path)
+                .with_arg("--id")
+                .with_arg($id)
+                .output()
+                .expect("execute command");
+
+            // for ease of debug
+            eprintln!("STDERR:\n{}", u8v_to_string(&stderr));
+            eprintln!("STDOUT:\n{}", u8v_to_string(&stdout));
+
+            // basic guarantees
+            assert!(!status.success());
+            assert!(stdout.is_empty());
+
+            // compare stderr
+            assert_eq!(u8v_to_string(&stderr), $expected);
+        }
+    };
+}
+
+picture_file_id_fail!(picture_file_0_empty0: "audio0" --id="0" => "error: Specified picture ID is out of bound\n");
+picture_file_id_fail!(picture_file_0_empty1: "audio1" --id="1" => "error: Specified picture ID is out of bound\n");
+picture_file_id!(picture_file_0_filled2: "audio2" --id="0" => "98efb430f0e307315ee46a81bfaf4ba9cf79e5996dcd227a306e1aaaf438cda4");
+picture_file_id_fail!(picture_file_1_filled2: "audio2" --id="1" => "error: Specified picture ID is out of bound\n");
+picture_file_id!(picture_file_0_filled3: "audio3" --id="0" => "668c0693f36c2c08f8a04fd09cf9dcf38d14a52f2d65134077939a62b363d48a");
+picture_file_id!(picture_file_1_filled3: "audio3" --id="1" => "deaad585bd1cdbf05011c88a10cff00e630878dbc1408c1c365c19ba8ee5e169");
+picture_file_id!(picture_file_2_filled3: "audio3" --id="2" => "96c87d647f1be8168d7b52198fe80345808e2cde8fac30733887abc9414c5a4a");
+picture_file_id!(picture_file_3_filled3: "audio3" --id="3" => "ff1b6b1c8a2fcb256b2f6ac5f8678dc4d185fe652d2815364b1f268475bbd4c4");
+picture_file_id_fail!(picture_file_4_filled3: "audio3" --id="4" => "error: Specified picture ID is out of bound\n");
+
 macro_rules! picture_dir_empty {
     (
         $(#[$attributes:meta])*
