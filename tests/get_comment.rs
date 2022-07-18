3e6b434965e15712abece650e61d1085d89eba93
khai96_ <hvksmr1996@gmail.com>
2022-07-18 18:41:56 +0700

    test: refactor: Merge `_format` variants

--- tests/get_comment.rs
+++ tests/get_comment.rs
@@ -8,6 +8,7 @@ use serde_json::{json, Value as JsonValue};
 use std::process::Output;
 
 macro_rules! comment {
+    // Without --format
     (
         $(#[$attributes:meta])*
         $name:ident:
@@ -49,6 +50,66 @@ macro_rules! comment {
 
             // test stdout
             assert_eq!(u8v_to_string(&stdout), $expected);
+        }
+    };
+
+    // With --format
+    (
+        $(#[$attributes:meta])*
+        $name:ident:
+        $audio_name:literal
+        --format=$format:ident
+        $(--lang=$lang:ident)?
+        $(--description=$description:ident)?
+        => $expected:expr
+    ) => {
+        #[test]
+        fn $name() {
+            let expected = $expected;
+
+            let audio_path = assets().join($audio_name);
+            let Output {
+                status,
+                stdout,
+                stderr,
+            } = Exe::project_root()
+                .cmd
+                .with_arg("get")
+                .with_arg("comment")
+                $(
+                    .with_arg("--lang")
+                    .with_arg(stringify!($lang))
+                )?
+                $(
+                    .with_arg("--description")
+                    .with_arg(stringify!($description))
+                )?
+                .with_arg("--format")
+                .with_arg(stringify!($format))
+                .with_arg(audio_path)
+                .output()
+                .expect("execute command");
+
+            // for ease of debug
+            eprintln!("STDOUT:\n{}", u8v_to_string(&stdout));
+            eprintln!("STDERR:\n{}", u8v_to_string(&stderr));
+
+            // basic guarantees
+            assert!(status.success());
+            assert!(stderr.is_empty());
+
+            // test the structured information
+            let received = stdout
+                .pipe_as_ref(u8v_to_string)
+                .pipe(deserialize::$format::<JsonValue>)
+                .expect("deserialize value");
+            assert_eq!(received, expected);
+
+            // assert that the output text is prettified
+            let received = u8v_to_string(&stdout);
+            let expected = serialize::$format(&expected).expect("serialize value");
+            let expected = format!("{expected}\n");
+            assert_eq!(received, expected);
         }
     };
 }
@@ -117,73 +178,12 @@ comment_fail!(#[cfg(unix)] comment_dir: "." => format!(
     assets().join("."),
 ));
 
-macro_rules! comment_format {
-    (
-        $(#[$attributes:meta])*
-        $name:ident:
-        $audio_name:literal
-        --format=$format:ident
-        $(--lang=$lang:ident)?
-        $(--description=$description:ident)?
-        => $expected:expr
-    ) => {
-        #[test]
-        fn $name() {
-            let expected = $expected;
+comment!(comment_json_empty0: "audio0" --format=json => json!([]));
+comment!(comment_yaml_empty0: "audio0" --format=yaml => json!([]));
+comment!(comment_json_empty1: "audio1" --format=json => json!([]));
+comment!(comment_yaml_empty1: "audio1" --format=yaml => json!([]));
 
-            let audio_path = assets().join($audio_name);
-            let Output {
-                status,
-                stdout,
-                stderr,
-            } = Exe::project_root()
-                .cmd
-                .with_arg("get")
-                .with_arg("comment")
-                $(
-                    .with_arg("--lang")
-                    .with_arg(stringify!($lang))
-                )?
-                $(
-                    .with_arg("--description")
-                    .with_arg(stringify!($description))
-                )?
-                .with_arg("--format")
-                .with_arg(stringify!($format))
-                .with_arg(audio_path)
-                .output()
-                .expect("execute command");
-
-            // for ease of debug
-            eprintln!("STDOUT:\n{}", u8v_to_string(&stdout));
-            eprintln!("STDERR:\n{}", u8v_to_string(&stderr));
-
-            // basic guarantees
-            assert!(status.success());
-            assert!(stderr.is_empty());
-
-            // test the structured information
-            let received = stdout
-                .pipe_as_ref(u8v_to_string)
-                .pipe(deserialize::$format::<JsonValue>)
-                .expect("deserialize value");
-            assert_eq!(received, expected);
-
-            // assert that the output text is prettified
-            let received = u8v_to_string(&stdout);
-            let expected = serialize::$format(&expected).expect("serialize value");
-            let expected = format!("{expected}\n");
-            assert_eq!(received, expected);
-        }
-    };
-}
-
-comment_format!(comment_json_empty0: "audio0" --format=json => json!([]));
-comment_format!(comment_yaml_empty0: "audio0" --format=yaml => json!([]));
-comment_format!(comment_json_empty1: "audio1" --format=json => json!([]));
-comment_format!(comment_yaml_empty1: "audio1" --format=yaml => json!([]));
-
-comment_format!(comment_json_filled2: "audio2" --format=json => json!([
+comment!(comment_json_filled2: "audio2" --format=json => json!([
     {
         "description": "",
         "lang": "jpn",
@@ -191,7 +191,7 @@ comment_format!(comment_json_filled2: "audio2" --format=json => json!([
     },
 ]));
 
-comment_format!(comment_yaml_filled2: "audio2" --format=yaml => json!([
+comment!(comment_yaml_filled2: "audio2" --format=yaml => json!([
     {
         "description": "",
         "lang": "jpn",
@@ -199,7 +199,7 @@ comment_format!(comment_yaml_filled2: "audio2" --format=yaml => json!([
     },
 ]));
 
-comment_format!(comment_json_filled3: "audio3" --format=json => json!([
+comment!(comment_json_filled3: "audio3" --format=json => json!([
     {
         "description": "",
         "lang": "eng",
@@ -212,7 +212,7 @@ comment_format!(comment_json_filled3: "audio3" --format=json => json!([
     },
 ]));
 
-comment_format!(comment_json_jpn_filled3: "audio3" --format=json --lang=jpn => json!([
+comment!(comment_json_jpn_filled3: "audio3" --format=json --lang=jpn => json!([
     {
         "description": "",
         "lang": "jpn",
@@ -220,7 +220,7 @@ comment_format!(comment_json_jpn_filled3: "audio3" --format=json --lang=jpn => j
     },
 ]));
 
-comment_format!(comment_yaml_filled3: "audio3" --format=yaml => json!([
+comment!(comment_yaml_filled3: "audio3" --format=yaml => json!([
     {
         "description": "",
         "lang": "eng",
@@ -233,7 +233,7 @@ comment_format!(comment_yaml_filled3: "audio3" --format=yaml => json!([
     },
 ]));
 
-comment_format!(comment_yaml_eng_filled3: "audio3" --format=yaml --lang=eng => json!([
+comment!(comment_yaml_eng_filled3: "audio3" --format=yaml --lang=eng => json!([
     {
         "description": "",
         "lang": "eng",
--- tests/get_text.rs
+++ tests/get_text.rs
@@ -7,6 +7,55 @@ use pretty_assertions::assert_eq;
 use std::process::Output;
 
 macro_rules! text {
+    // With --format
+    (
+        $(#[$attributes:meta])*
+        $name:ident: $field:literal --format=$format:ident $audio_name:literal => $expected:expr
+    ) => {
+        $(#[$attributes])*
+        #[test]
+        fn $name() {
+            let audio_path = assets().join($audio_name);
+            let Output {
+                status,
+                stdout,
+                stderr,
+            } = Exe::project_root()
+                .cmd
+                .with_arg("get")
+                .with_arg($field)
+                .with_arg("--format")
+                .with_arg(stringify!($format))
+                .with_arg(audio_path)
+                .output()
+                .expect("execute command");
+
+            // for ease of debug
+            eprintln!("STDERR:\n{}", u8v_to_string(&stderr));
+
+            // basic guarantees
+            assert!(status.success());
+            assert!(stderr.is_empty());
+
+            // test the structured information
+            let received = stdout
+                .pipe_as_ref(u8v_to_string)
+                .pipe(deserialize::$format::<Option<String>>)
+                .expect("deserialize value");
+            let expected = $expected.map(|x: &str| x.to_string());
+            assert_eq!(received, expected);
+
+            // assert that the output text is prettified
+            let received = u8v_to_string(&stdout);
+            let expected = $expected
+                .pipe_ref(serialize::$format)
+                .expect("serialize value");
+            let expected = format!("{expected}\n");
+            assert_eq!(received, expected);
+        }
+    };
+
+    // Without --format
     (
         $(#[$attributes:meta])*
         $name:ident: $field:literal $audio_name:literal => $stdout:expr
@@ -60,64 +109,15 @@ text!(genre_empty1: "genre" "audio1" => "");
 text!(genre_filled2: "genre" "audio2" => "Anime\n");
 text!(genre_filled3: "genre" "audio3" => "Pop\n");
 
-macro_rules! text_format {
-    (
-        $(#[$attributes:meta])*
-        $name:ident: $field:literal --format=$format:ident $audio_name:literal => $expected:expr
-    ) => {
-        $(#[$attributes])*
-        #[test]
-        fn $name() {
-            let audio_path = assets().join($audio_name);
-            let Output {
-                status,
-                stdout,
-                stderr,
-            } = Exe::project_root()
-                .cmd
-                .with_arg("get")
-                .with_arg($field)
-                .with_arg("--format")
-                .with_arg(stringify!($format))
-                .with_arg(audio_path)
-                .output()
-                .expect("execute command");
+text!(title_json_empty0: "title" --format=json "audio0" => None::<&str>);
+text!(title_json_empty1: "title" --format=json "audio1" => None::<&str>);
+text!(title_json_filled2: "title" --format=json "audio2" => Some("砕月"));
+text!(title_json_filled3: "title" --format=json "audio3" => Some("Broken Moon"));
 
-            // for ease of debug
-            eprintln!("STDERR:\n{}", u8v_to_string(&stderr));
-
-            // basic guarantees
-            assert!(status.success());
-            assert!(stderr.is_empty());
-
-            // test the structured information
-            let received = stdout
-                .pipe_as_ref(u8v_to_string)
-                .pipe(deserialize::$format::<Option<String>>)
-                .expect("deserialize value");
-            let expected = $expected.map(|x: &str| x.to_string());
-            assert_eq!(received, expected);
-
-            // assert that the output text is prettified
-            let received = u8v_to_string(&stdout);
-            let expected = $expected
-                .pipe_ref(serialize::$format)
-                .expect("serialize value");
-            let expected = format!("{expected}\n");
-            assert_eq!(received, expected);
-        }
-    };
-}
-
-text_format!(title_json_empty0: "title" --format=json "audio0" => None::<&str>);
-text_format!(title_json_empty1: "title" --format=json "audio1" => None::<&str>);
-text_format!(title_json_filled2: "title" --format=json "audio2" => Some("砕月"));
-text_format!(title_json_filled3: "title" --format=json "audio3" => Some("Broken Moon"));
-
-text_format!(artist_yaml_empty0: "artist" --format=yaml "audio0" => None::<&str>);
-text_format!(artist_yaml_empty1: "artist" --format=yaml "audio1" => None::<&str>);
-text_format!(artist_yaml_filled2: "artist" --format=yaml "audio2" => Some("ココ&さつき が てんこもり"));
-text_format!(artist_yaml_filled3: "artist" --format=yaml "audio3" => Some("Koko & Satsuki ga Tenkomori"));
+text!(artist_yaml_empty0: "artist" --format=yaml "audio0" => None::<&str>);
+text!(artist_yaml_empty1: "artist" --format=yaml "audio1" => None::<&str>);
+text!(artist_yaml_filled2: "artist" --format=yaml "audio2" => Some("ココ&さつき が てんこもり"));
+text!(artist_yaml_filled3: "artist" --format=yaml "audio3" => Some("Koko & Satsuki ga Tenkomori"));
 
 macro_rules! text_fail {
     (
