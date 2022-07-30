3addf853b737823c50fe8ea631f493afb02c455a
khai96_ <hvksmr1996@gmail.com>
2022-07-30 20:13:15 +0700

    test: `delete all --no-backup`

---
+++ tests/delete_all.rs
@@ -0,0 +1,47 @@
+pub mod _utils;
+
+use _utils::{u8v_to_string, Exe};
+use command_extra::CommandExtra;
+use id3_cli::utils::read_tag_from_path;
+use pretty_assertions::assert_eq;
+use std::process::Output;
+
+macro_rules! all {
+    // With --no-backup
+    ($(#[$attributes:meta])* $name:ident: $audio_name:literal --no-backup) => {
+        #[test]
+        fn $name() {
+            let Exe { cmd, wdir } = Exe::temp_workspace();
+            dbg!(&wdir);
+            let audio_path = wdir.join("assets").join($audio_name);
+            dbg!(&audio_path);
+            let Output {
+                status,
+                stdout,
+                stderr,
+            } = cmd
+                .with_arg("delete")
+                .with_arg("all")
+                .with_arg("--no-backup")
+                .with_arg(&audio_path)
+                .output()
+                .expect("execute command");
+
+            // for ease of debug
+            eprintln!("STDERR:\n{}", u8v_to_string(&stderr));
+            eprintln!("STDOUT:\n{}", u8v_to_string(&stdout));
+
+            // basic guarantees
+            assert!(status.success());
+
+            // assert that there are no tag
+            let tag = read_tag_from_path(&audio_path).expect("read tag from path");
+            assert_eq!(tag.frames().next(), None);
+        }
+    };
+}
+
+all!(delete_no_backup_empty0: "audio0" --no-backup);
+all!(delete_no_backup_empty1: "audio1" --no-backup);
+all!(delete_no_backup_filled2: "audio2" --no-backup);
+all!(delete_no_backup_filled3: "audio3" --no-backup);
