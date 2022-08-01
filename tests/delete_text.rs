75e240cfaf05a82b4c8f63b818fd48f719fefb4d
khai96_ <hvksmr1996@gmail.com>
2022-08-01 15:29:46 +0700

    test: `delete text --no-backup`

---
+++ tests/delete_text.rs
@@ -0,0 +1,55 @@
+pub mod _utils;
+
+use _utils::{u8v_to_string, Exe};
+use command_extra::CommandExtra;
+use id3::{Tag, TagLike};
+use pretty_assertions::assert_eq;
+use std::process::Output;
+
+macro_rules! delete_text {
+    // With --no-backup
+    (
+        $(#[$attributes:meta])*
+        $name:ident: $field:literal --no-backup $audio_name:literal => $method:ident
+    ) => {
+        $(#[$attributes])*
+        #[test]
+        fn $name() {
+            let Exe { cmd, wdir } = Exe::temp_workspace();
+            let audio_path = wdir.join("assets").join($audio_name);
+            dbg!(&audio_path);
+            let Output {
+                status,
+                stdout,
+                stderr,
+            } = cmd
+                .with_arg("delete")
+                .with_arg($field)
+                .with_arg("--no-backup")
+                .with_arg(&audio_path)
+                .output()
+                .expect("execute command");
+
+            // for ease of debug
+            eprintln!("STDERR:\n{}", u8v_to_string(&stderr));
+            eprintln!("STDOUT:\n{}", u8v_to_string(&stdout));
+
+            // basic guarantees
+            assert!(status.success());
+            assert!(stdout.is_empty());
+
+            // assert that the text field is deleted.
+            let tag = Tag::read_from_path(&audio_path).expect("read tag from audio");
+            assert_eq!(tag.$method(), None);
+
+            // make sure that no backup was created
+            assert!(wdir.join("assets").exists());
+            assert!(!wdir.join("assets").join(".id3-backups").exists());
+        }
+    };
+}
+
+delete_text!(title_no_backup_audio0: "title" --no-backup "audio0" => title);
+delete_text!(artist_no_backup_audio1: "artist" --no-backup "audio1" => artist);
+delete_text!(album_no_backup_audio2: "album" --no-backup "audio2" => album);
+delete_text!(genre_no_backup_audio3: "genre" --no-backup "audio3" => genre);
