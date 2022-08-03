47a84bb5daf56f2ad80ef790ce9272cf80adb584
khai96_ <hvksmr1996@gmail.com>
2022-08-03 17:20:53 +0700

    test: `delete <text-field>` (without --no-backup)

--- tests/delete_text.rs
+++ tests/delete_text.rs
@@ -1,6 +1,7 @@
 pub mod _utils;
 
-use _utils::{u8v_to_string, Exe};
+use _utils::{sha256_file, u8v_to_string, Exe, TestBackup};
+use chrono::Local;
 use command_extra::CommandExtra;
 use id3::{Tag, TagLike};
 use pretty_assertions::assert_eq;
@@ -47,9 +48,63 @@ macro_rules! delete_text {
             assert!(!wdir.join("assets").join(".id3-backups").exists());
         }
     };
+
+    // Without --no-backup
+    (
+        $(#[$attributes:meta])*
+        $name:ident: $field:literal $audio_name:literal => $method:ident
+    ) => {
+        $(#[$attributes])*
+        #[test]
+        fn $name() {
+            let Exe { cmd, wdir } = Exe::temp_workspace();
+            let audio_path = wdir.join("assets").join($audio_name);
+            dbg!(&audio_path);
+            let initial_hash = sha256_file(&audio_path);
+            dbg!(&initial_hash);
+            let before_run = Local::now();
+            dbg!(before_run);
+            let Output {
+                status,
+                stdout,
+                stderr,
+            } = cmd
+                .with_arg("delete")
+                .with_arg($field)
+                .with_arg(&audio_path)
+                .output()
+                .expect("execute command");
+
+            // for ease of debug
+            eprintln!("STDERR:\n{}", u8v_to_string(&stderr));
+            eprintln!("STDOUT:\n{}", u8v_to_string(&stdout));
+
+            // basic guarantees
+            assert!(status.success());
+            assert!(stdout.is_empty());
+
+            // assert that the text field is deleted.
+            let tag = Tag::read_from_path(&audio_path).expect("read tag from audio");
+            assert_eq!(tag.$method(), None);
+
+            // make sure that a backup was created
+            TestBackup::builder()
+                .workspace(&wdir)
+                .audio_name($audio_name)
+                .initial_hash(&initial_hash)
+                .before_run(before_run)
+                .build()
+                .test();
+        }
+    };
 }
 
 delete_text!(title_no_backup_audio0: "title" --no-backup "audio0" => title);
 delete_text!(artist_no_backup_audio1: "artist" --no-backup "audio1" => artist);
 delete_text!(album_no_backup_audio2: "album" --no-backup "audio2" => album);
 delete_text!(genre_no_backup_audio3: "genre" --no-backup "audio3" => genre);
+
+delete_text!(title_audio0: "title" "audio0" => title);
+delete_text!(artist_audio1: "artist" "audio1" => artist);
+delete_text!(album_audio2: "album" "audio2" => album);
+delete_text!(genre_audio3: "genre" "audio3" => genre);
