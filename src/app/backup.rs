1087831954e597634cbb10b8aa5a0ab649b5cbed
khai96_ <hvksmr1996@gmail.com>
2022-08-07 19:11:20 +0700

    refactor: Run backup directly

--- src/app/backup.rs
+++ src/app/backup.rs
@@ -1,6 +1,11 @@
-use crate::{app::Run, error::Error, utils::ModifyTags};
+use crate::{
+    app::Run,
+    backup,
+    error::{Error, FileReadFailure},
+    utils::sha256_data,
+};
 use clap::Args;
-use std::path::PathBuf;
+use std::{fs::read as read_file, path::PathBuf};
 
 #[derive(Debug, Args)]
 pub struct Backup {
@@ -10,10 +15,19 @@ pub struct Backup {
 
 impl Run for Backup {
     fn run(self) -> Result<(), Error> {
-        ModifyTags::builder()
-            .no_backup(false)
-            .target_audio(&self.target_audio)
+        let Backup { ref target_audio } = self;
+
+        let audio_content = read_file(target_audio).map_err(|error| FileReadFailure {
+            file: target_audio.to_path_buf(),
+            error,
+        })?;
+
+        backup::Backup::builder()
+            .source_file_path(target_audio)
+            .source_file_hash(&sha256_data(&audio_content))
             .build()
-            .run(|_| ())
+            .backup()?;
+
+        Ok(())
     }
 }
