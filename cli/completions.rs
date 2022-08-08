fa9327a27000761ba1574499b78461a2283bd128
khai96_ <hvksmr1996@gmail.com>
2022-08-08 12:43:23 +0700

    refactor: Use `clap-utilities`

--- cli/completions.rs
+++ cli/completions.rs
@@ -1,63 +1,7 @@
-use clap::{CommandFactory, Parser};
-use clap_complete::{generate, Shell};
+use clap_utilities::CommandFactoryExtra;
 use id3_cli::app::App;
-use std::{fs::File, io, path::PathBuf, process::ExitCode};
-use thiserror::Error;
-
-/// Generate completions.
-#[derive(Debug, Parser)]
-struct ComGen {
-    /// Bin name.
-    #[clap(long, short)]
-    name: String,
-    /// Target shell.
-    #[clap(long, short, value_enum)]
-    shell: Shell,
-    /// Output file.
-    #[clap(long, short)]
-    output: PathBuf,
-}
-
-/// Error of the generator.
-#[derive(Debug, Error)]
-enum Error {
-    #[error("{}: {error}", file.to_string_lossy())]
-    FileSystem {
-        file: PathBuf,
-        #[source]
-        error: io::Error,
-    },
-}
-
-impl ComGen {
-    /// Run the generator.
-    fn run(self) -> Result<(), Error> {
-        let ComGen {
-            name,
-            shell,
-            output,
-        } = self;
-        let mut cmd = App::command();
-        let mut output_file = File::create(&output).map_err(|error| Error::FileSystem {
-            file: output.clone(),
-            error,
-        })?;
-        generate(shell, &mut cmd, name, &mut output_file);
-        Ok(())
-    }
-
-    /// The program that generates shell completions.
-    fn main() -> ExitCode {
-        match ComGen::parse().run() {
-            Ok(()) => ExitCode::SUCCESS,
-            Err(error) => {
-                eprintln!("{error}");
-                ExitCode::FAILURE
-            }
-        }
-    }
-}
+use std::process::ExitCode;
 
 fn main() -> ExitCode {
-    ComGen::main()
+    App::run_completion_generator()
 }
